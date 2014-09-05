db75468f68e1b1300d00c035e5235b0db49d5265
wuduoyi <duoyi.wu@gmail.com>
2014-09-05 17:01:05 +0800

    生产环境使用 8999 端口

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,15 +3,10 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
-
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            8080,
+            8999,
 
   // MongoDB connection options
   mongo: {
